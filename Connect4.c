7fdd5674c77791eba844bcfec0f9d00c3f53b82a
Patategrill <194829329+patategrill@users.noreply.github.com>
2026-09-12 17:17:48 +0200

    Implement empty grid and display grid functions

---
+++ Connect4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int grid[6][7];
+int i,j;
+
+int empty_grid() {
+    
+    for (i=0;i<7;i++)
+    {
+        for (j=0;i<6;i++)
+        {
+            grid[i][j]=0;
+        }
+    }
+}    
+
+int display_grid() {
+    for (i=0,i<,i++)
+
+    printf('\n');
+}
